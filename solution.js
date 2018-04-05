52bfdd801a917a79ba597432d3ba3ddb94bb8c83
Mariana Casella dos Santos <mariana.casella@88solutions.be>
2018-04-05 15:59:32 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,3 +1,13 @@
 // Your code here
 
+var repeatedString;
+function stringRepeat (string, val) {
+  repeatedString = string;
+  for (i=0, i < val, i++) {
+    repeatedString += string;
+    return repeatedString;
+  }
+
+
+
 // Be sure it's the cleanest code you can write!
